dc79f802d04caca3efbcc32bc3a4e6325620fc5e
Bargav Kondapu <bargav_kondapu@fanniemae.com>
2022-05-29 23:17:43 -0400

    Create Ajax form displaying word entered.

---
+++ src/Form/CRLDictionaryForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\crl_dictionary\Form;
+
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
+
+/**
+ * Implements an example form.
+ */
+class CRLDictionaryForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'crl_dictionary_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['field_word'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter word here'),
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+      '#button_type' => 'primary',
+      '#ajax' => [
+        'callback' => '::getDefinitions'
+      ]
+    ];
+
+    $form['field_definitions'] = [
+      '#type' => 'markup',
+      '#markup' => '<div class="result_message"></div>'
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefinitions(array &$form, FormStateInterface $form_state) {
+
+    $response = new AjaxResponse();
+    $response->addCommand(
+      new HtmlCommand(
+        '.result_message',
+        '<div class="my_top_message">' . t('Word is @word', ['@word' => ($form_state->getValue('field_word'))]) . '</div>'),
+    );
+    return $response;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+}
